bd170dc415931a0a6d68c2bfdc0abf10416d59d5
Dipssy <briancoppin1224@gmail.com>
2025-03-08 22:37:12 +0200

    completed exercise 1.5
    completed Course Information (exercise 1.1-1.5)

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -10,16 +10,16 @@ const Content = (props) => {
 	return (
 		<div>
 			<Part
-				name={props.parts[0].name}
-				exercises={props.parts[0].exercises}
+				name={props.course.parts[0].name}
+				exercises={props.course.parts[0].exercises}
 			/>
 			<Part
-				name={props.parts[1].name}
-				exercises={props.parts[1].exercises}
+				name={props.course.parts[1].name}
+				exercises={props.course.parts[1].exercises}
 			/>
 			<Part
-				name={props.parts[2].name}
-				exercises={props.parts[2].exercises}
+				name={props.course.parts[2].name}
+				exercises={props.course.parts[2].exercises}
 			/>
 		</div>
 	);
