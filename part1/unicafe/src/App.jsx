00eb8752d8bd4322b27f17cf06f9cf10a77dcfe5
Dipssy <briancoppin1224@gmail.com>
2025-03-10 11:33:13 +0200

    exercise 1.10 of part1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from "react";
 
 const Statistics = (props) => {
+	const average = (props.good - props.bad) / props.allClicks;
+	const positive = (props.good / props.allClicks) * 100;
+
 	if (props.allClicks > 0) {
 		return (
 			<div>
-				<p>good 6</p>
-				<p>neutral 2</p>
-				<p>bad 1</p>
-				<p>all 9</p>
-				<p>average 0.5555555556</p>
-				<p>positive 0.66666666667%</p>
+				<StatisticsLine
+					text="good"
+					value={props.good}
+				/>
+				<StatisticsLine
+					text="neutral"
+					value={props.neutral}
+				/>
+				<StatisticsLine
+					text="bad"
+					value={props.bad}
+				/>
+				<StatisticsLine
+					text="all"
+					value={props.allClicks}
+				/>
+				<StatisticsLine
+					text="average"
+					value={average}
+				/>
+				<StatisticsLine
+					text="positive"
+					value={`${positive}%`}
+				/>
 			</div>
 		);
 	}
@@ -28,6 +49,18 @@ const Statistics = (props) => {
 	// );
 };
 
+const Button = (props) => {
+	return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const StatisticsLine = (props) => {
+	return (
+		<p>
+			{props.text} {props.value}
+		</p>
+	);
+};
+
 const App = () => {
 	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
@@ -35,18 +68,47 @@ const App = () => {
 	const [bad, setBad] = useState(0);
 	const [allClicks, setAll] = useState(0);
 
+	const incrementGood = () => {
+		setGood(good + 1);
+		setAll(allClicks + 1);
+	};
+
+	const incrementNeutral = () => {
+		setNeutral(neutral + 1);
+		setAll(allClicks + 1);
+	};
+
+	const incrementBad = () => {
+		setBad(bad + 1);
+		setAll(allClicks + 1);
+	};
+
 	return (
 		<div>
 			<h1>give feedback</h1>
-			<button>good</button>
-			<button>neutral</button>
-			<button>bad</button>
+			<Button
+				text="good"
+				handleClick={incrementGood}
+			/>
+			<Button
+				text="neutral"
+				handleClick={incrementNeutral}
+			/>
+			<Button
+				text="bad"
+				handleClick={incrementBad}
+			/>
 			<h1>statistics</h1>
 			{
 				// could use ternary operator for conditional rendering
 				// {allClicks > 0 ? <Statistics /> : "No feedback given"}
 			}
-			<Statistics />
+			<Statistics
+				good={good}
+				neutral={neutral}
+				bad={bad}
+				allClicks={allClicks}
+			/>
 		</div>
 	);
 };
