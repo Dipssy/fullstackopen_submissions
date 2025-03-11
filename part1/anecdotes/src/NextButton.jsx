95092704d590a603ccc00a4f13541b071b9082a9
Dipssy <briancoppin1224@gmail.com>
2025-03-11 16:53:52 +0200

    completed exercise 1.12 of part1

---
+++ part1/anecdotes/src/NextButton.jsx
@@ -0,0 +1,9 @@
+/**
+ * renders the button that selects a random anecdote when clicked
+ * props have been destructured for the button's text and event handler
+ */
+const NextButton = ({ text, handleClick }) => {
+	return <button onClick={handleClick}>{text}</button>;
+};
+
+export default NextButton;
